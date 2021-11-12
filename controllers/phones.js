196148a00bd32216580a87984d6c1a9adf33ccbc
nagireddyakhilredddy <S545143@NWMISSOURI.EDU>
2021-11-12 01:36:21 -0600

    changes made

--- controllers/phones.js
+++ controllers/phones.js
@@ -1,63 +1,62 @@
 var phones = require('../models/phones'); 
  
-// List of all phoness 
+// List of all Costumes 
 exports.phones_list = function(req, res) { 
-    res.send('NOT IMPLEMENTED: phones list'); 
+    res.send('NOT IMPLEMENTED: Costume list'); 
 }; 
  
-// for a specific phones. 
+// for a specific Costume. 
 exports.phones_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: phones detail: ' + req.params.id); 
+    res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id); 
 }; 
  
-// Handle phones create on POST. 
+// Handle Costume create on POST. 
 exports.phones_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: phones create POST'); 
+    res.send('NOT IMPLEMENTED: Costume create POST'); 
 }; 
  
-// Handle phones delete form on DELETE. 
+// Handle Costume delete form on DELETE. 
 exports.phones_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: phones delete DELETE ' + req.params.id); 
+    res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id); 
 }; 
  
-// Handle phones update form on PUT. 
+// Handle Costume update form on PUT. 
 exports.phones_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: phones update PUT' + req.params.id); 
+    res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id); 
 }; 
-
-// List of all phoness 
+// List of all Costumes 
 exports.phones_list = async function(req, res) { 
     try{ 
-        thephoness = await phones.find(); 
-        res.send(thephoness); 
+        theCostumes = await phones.find(); 
+        res.send(theCostumes); 
     } 
     catch(err){ 
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
 }; 
-// VIEWS 
+ // VIEWS 
 // Handle a show all view 
 exports.phones_view_all_Page = async function(req, res) { 
     try{ 
-        thephoness = await phones.find(); 
-        res.render('phones', { title: 'phones Search Results', results: thephoness }); 
+        theCostumes = await phones.find(); 
+        res.render('phones', { title: 'phones Search Results', results: theCostumes }); 
     } 
     catch(err){ 
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
 }; 
-// Handle phones create on POST. 
+// Handle Costume create on POST. 
 exports.phones_create_post = async function(req, res) { 
     console.log(req.body) 
     let document = new phones(); 
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"phones_type":"goat", "cost":12, "size":"large"} 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
     document.pName = req.body.pName; 
-    document.pPixels = req.body.pPixels; 
+    document.pEngine = req.body.pEngine; 
     document.pCost = req.body.pCost; 
     try{ 
         let result = await document.save(); 
@@ -67,4 +66,16 @@ exports.phones_create_post = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
+};
+
+// for a specific Costume. 
+exports.phones_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Costume.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
--- routes/phones.js
+++ routes/phones.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('phones', { title: 'Search Results phones' });
-});
-
+var express = require('express'); 
+const phones_controlers= require('../controllers/phones'); 
+var router = express.Router(); 
+ 
+/* GET costumes */ 
+router.get('/', phones_controlers.phones_view_all_Page ); 
 module.exports = router;
+// GET request for one costume. 
+router.get('/costumes/:id', phones_controller.phones_detail); 
