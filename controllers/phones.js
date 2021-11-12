0669f5dff95d931629fd5623f411886c4b4c3fb5
nagireddyakhilredddy <S545143@NWMISSOURI.EDU>
2021-11-12 02:12:40 -0600

    changes made

--- controllers/phones.js
+++ controllers/phones.js
@@ -79,3 +79,24 @@ exports.phones_detail = async function(req, res) {
         res.send(`{"error": document for id ${req.params.id} not found`); 
     } 
 }; 
+
+// Handle Costume update form on PUT. 
+exports.phones_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await phones.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.pName)  
+               toUpdate.pName = req.body.pName; 
+        if(req.body.pPixels) toUpdate.pPixels = req.body.pPixels; 
+        if(req.body.pCost) toUpdate.pCost = req.body.pCost; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
